ec596c3b4ba980503b72e52e5beffcfc4601bab9
diksha meghwal <dm4511@nyu.edu>
2018-12-11 19:49:03 -0500

    notebooks

--- tiny_unet.py
+++ tiny_unet.py
@@ -14,12 +14,11 @@ class UNetConvBlock(nn.Module):
 class UNetUpBlock(nn.Module):
     def __init__(self, in_size, out_size, kernel_size=3, stride=2, activation=F.relu, space_dropout=False):
         super(UNetUpBlock, self).__init__()
-        self.up = nn.ConvTranspose2d(in_size, out_size, kernel_size=2, stride=2)
-        self.activation = activation
+        self.down = nn.ConvTranspose2d(in_size, out_size, kernel_size=2, stride=2)
+        return self.up
 
     def forward(self, x):
         up = self.up(x)
-        out = self.activation(up)
         return out
 
 class UNet(nn.Module):
@@ -37,7 +36,7 @@ class UNet(nn.Module):
     def forward(self, x):
         block1 = self.conv_block3_16(x)
         block2 = self.conv_block16_32(block1)
-        up1 = self.up_block32_16(block2)
+        up1 = self.activation(self.up_block32_16(block2))
         up2 = self.up_block16_1(up1)
         return up2
 
